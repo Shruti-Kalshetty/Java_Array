4747bc3ec41ed50594d0c16b47f48fbf23a14d3a
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-12-02 18:31:05 +0530

    Add files via upload

---
+++ CreateArray.java
@@ -0,0 +1,10 @@
+public class CreateArray {
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 3, 4, 5, 6 };
+        System.out.println("Array elements: ");
+
+        for (int i = 0; i <= a.length; i++) {
+            System.out.println(i);
+        }
+    }
+}
